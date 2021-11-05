a3fa8e398a07871741749815b520a9dbb8e2c020
Zachary Parrish <zachary@parrish.io>
2021-11-05 09:37:43 -0400

    Updated voting list

---
+++ src/components/SelectedTokens.tsx
@@ -0,0 +1,24 @@
+import { Box, ListItem, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { TokenData } from 'providers/Token'
+
+const SelectedTokens = (props: {
+  selectedTokens: TokenData[]
+  handleOnRemoveToken(token: TokenData): void
+  handleOnVote(address: string, vote: number): void
+}) => {
+  useEffect(() => {}, [])
+
+  return (
+    <Box>
+      {props.selectedTokens.map((token) => (
+        <ListItem>
+          <img loading='lazy' width='20' src={token.logoURI} alt='' />
+          {token.name} ({token.symbol})<Text> Voting here</Text>
+        </ListItem>
+      ))}
+    </Box>
+  )
+}
+
+export default SelectedTokens
--- src/components/VoteList.tsx
+++ src/components/VoteList.tsx
@@ -1,34 +1,59 @@
+import { Box, Button, ListItem, Text } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Autocomplete from '@mui/material/Autocomplete'
+import { Autocomplete } from '@mui/material'
 import { createFilterOptions } from '@mui/material/Autocomplete'
 
-import { useMaxComponents } from 'hooks/votingContract'
+import { useMaxComponents, useVote } from 'hooks/votingContract'
+import SelectedTokens from './SelectedTokens'
+import { BigNumber } from '@ethersproject/bignumber'
 
 const VoteList = () => {
   const [tokens, setTokens] = useState<TokenData[]>([])
   const [selectedTokens, setSelectedTokens] = useState<TokenData[]>([])
-
-  const removeToken = (token: TokenData) => {
-    if (token)
-      setSelectedTokens(
-        selectedTokens.filter((selected) => selected.address !== token.address)
-      )
-  }
-
-  const addToken = (token: TokenData) => {
-    if (token) setSelectedTokens(selectedTokens.concat(token))
-  }
+  const [votes, setVotes] = useState<TokenVote[]>([])
 
   useEffect(() => {
     axios
       .get('https://tokens.coingecko.com/uniswap/all.json')
       .then((response) => {
+        console.log('Token Response', response)
         setTokens(response.data.tokens)
       })
   }, [])
+
+  const handleOnSelect = (item: TokenData) => {
+    let newSelected = selectedTokens.concat(item)
+    setSelectedTokens(newSelected)
+    console.log('selected', item, selectedTokens)
+  }
+
+  const handleOnRemoveToken = (removedToken: TokenData) => {
+    const remainingSelected = selectedTokens.filter(
+      (token) => token !== removedToken
+    )
+    const remainingVotes = votes.filter(
+      (vote) => vote.address !== removedToken.address
+    )
+    setSelectedTokens(remainingSelected)
+    setVotes(remainingVotes)
+    console.log('removed', removedToken, remainingVotes, remainingSelected)
+  }
+
+  const handleOnVote = (address: string, tokenVotes: number) => {
+    const vote: TokenVote = { address: address, votes: tokenVotes }
+    setVotes(votes.concat(vote))
+    console.log('voted', vote, votes)
+  }
+
+  const handleOnSubmit = () => {
+    const addresses = votes.map((vote) => vote.address)
+    const finalVotes = votes.map((vote) => BigNumber.from(vote.votes))
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useVote(addresses, finalVotes)
+    console.log('voted', addresses, finalVotes)
+  }
+
   return (
     <Box>
       <Autocomplete
@@ -43,22 +68,17 @@ const VoteList = () => {
           limit: 100,
         })}
         onChange={(_, value) => {
-          if (value != null) addToken(value)
+          if (value != null) handleOnSelect(value)
         }}
         getOptionLabel={(option) => option.name}
         renderOption={(props, option) => (
-          <Box
-            component='li'
-            sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
-            {...props}
-            key={option.address}
-          >
+          <ListItem>
             <img loading='lazy' width='20' src={option.logoURI} alt='' />
             {option.name} ({option.symbol}) - {option.address}
-          </Box>
+          </ListItem>
         )}
         renderInput={(params) => (
-          <TextField
+          <Text
             {...params}
             label='Choose a token'
             inputProps={{
@@ -68,17 +88,30 @@ const VoteList = () => {
           />
         )}
       />
+      <SelectedTokens
+        selectedTokens={selectedTokens}
+        handleOnVote={handleOnVote}
+        handleOnRemoveToken={handleOnRemoveToken}
+      />
+      <Button colorScheme='telegram' variant='solid' onClick={handleOnSubmit}>
+        Give a Hoot
+      </Button>
     </Box>
   )
 }
 
 export default VoteList
 
-interface TokenData {
+export interface TokenData {
   chainId: number
   address: string
   name: string
   symbol: string
   decimals: number
   logoURI: string
+}
+
+export interface TokenVote {
+  address: string
+  votes: number
 }
